e91c6d29bf2eb892df1eff54f1ec80338c622af5
eshop高晓东 <gaoxd3@asiainfo.com>
2019-02-19 10:58:20 +0800

    更改一下图片

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
+    <input type="text" v-model="msg">
     <h1>{{ msg }}</h1>
     <img :src="url">
-    <input type="text" v-model="msg">
   </div>
 </template>
 
 <script>
 
-import getData from './util';
+import action from './util';
 
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js',
-      url: require('./img/logo.png')
+      url: require('./img/boy.jpg')
     }
   },
   created() {
@@ -24,7 +24,7 @@ export default {
   },
   methods: {
      async fetchData() {
-      const data = await getData();
+      const data = await action.getData();
       this.msg = data;
     }
   }
